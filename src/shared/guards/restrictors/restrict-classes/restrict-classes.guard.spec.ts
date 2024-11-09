8efa0eb94bedd6cade2f726ed6337ebb7e231f9d
Rakesh Das <rakesh.das@digitalwoods.com>
2024-11-09 12:02:38 +0530

    restrictor guard and validator fix

--- src/shared/guards/restrictors/restrict-classes/restrict-classes.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RestrictClassesGuard } from './restrict-classes.guard';
-
-describe('RestrictClassesGuard', () => {
-  it('should be defined', () => {
-    expect(new RestrictClassesGuard()).toBeDefined();
-  });
-});
--- src/shared/guards/validators/permission-id/permission-id.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { PermissionIdGuard } from './permission-id.guard';
-
-describe('PermissionIdGuard', () => {
-  it('should be defined', () => {
-    expect(new PermissionIdGuard()).toBeDefined();
-  });
-});
